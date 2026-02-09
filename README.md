7b372dcce7f15b16d27323b31a245964ee15269b
EMosov <>
2026-02-10 00:08:03 +0300

    add flags to the program

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # C
 
 Well, `c` is THE BEST cli tool to clear your terminal!
+When you launch `c`, you'll see your dude at the top of the terminal, so you won't feel so lonely
+
+## Instalation
+
+To use this piece of art, just execute the following commands:
+```bash
+git clone https://github.com/EnotInc/c.git
+go install
+```
+
+And now you can use `C`!
+
+## Configuration
+
+Yeah! You read it correctly! You can config c to suit your needs!
+Here is a list of flags: 
+
+**Help**
+```bash
+c -h
+c --help
+```
+
+**Set your own dude**
+```bash
+c -d <string>
+c --dude <string>
+```
+
+**Set to default**
+```bash
+c --reset
+```
--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 const (
-	clear = "\033[2J\033[3J\033[0H"
-	dude  = " <( ._.)>"
+	clear       = "\033[2J\033[3J\033[0H"
+	defaultDude = "( ._.)"
 )
 
 func main() {
+	var userDude string
+	var reset bool
+
+	flag.StringVar(&userDude, "dude", "", "set you dude")
+	flag.StringVar(&userDude, "d", "", "set you dude")
+
+	flag.BoolVar(&reset, "reset", false, "reset you dude to default (use this flag on it's own)")
+
+	flag.Parse()
+
+	if reset {
+		os.WriteFile("dude.txt", []byte(defaultDude), 0644)
+		return
+	}
+
+	if userDude != "" {
+		os.WriteFile("dude.txt", []byte(userDude), 0644)
+	}
+
+	dude, err := os.ReadFile("dude.txt")
+	if err != nil {
+		fmt.Printf("%s\n\r", clear)
+		return
+	}
+
 	fmt.Printf("%s%s\n\r", clear, dude)
 }
