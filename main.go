10d1d6ba8e807350b00e95e81f3af47c0058bda1
EMosov <>
2026-02-10 00:25:11 +0300

    fix config

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 const (
@@ -11,7 +12,22 @@ const (
 	defaultDude = "( ._.)"
 )
 
+func getConfigPath() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return ".dude"
+	}
+	return filepath.Join(home, ".dude")
+}
+
 func main() {
+	config := getConfigPath()
+
+	if _, err := os.Stat(config); err != nil {
+		os.Create(config)
+		os.WriteFile(config, []byte(defaultDude), 0644)
+	}
+
 	var userDude string
 	var reset bool
 
@@ -23,15 +39,15 @@ func main() {
 	flag.Parse()
 
 	if reset {
-		os.WriteFile("dude.txt", []byte(defaultDude), 0644)
+		os.WriteFile(config, []byte(defaultDude), 0644)
 		return
 	}
 
 	if userDude != "" {
-		os.WriteFile("dude.txt", []byte(userDude), 0644)
+		os.WriteFile(config, []byte(userDude), 0644)
 	}
 
-	dude, err := os.ReadFile("dude.txt")
+	dude, err := os.ReadFile(config)
 	if err != nil {
 		fmt.Printf("%s\n\r", clear)
 		return
